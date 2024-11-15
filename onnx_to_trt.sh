d13b16537e50907186f15f4e783145e5b3746fe2
Your Name <you@example.com>
2024-11-15 01:35:33 +0000

    update

--- onnx_to_trt.sh
+++ onnx_to_trt.sh
@@ -2,4 +2,4 @@
 
 trtexec="/usr/src/tensorrt/bin/trtexec"
 
-$trtexec --onnx=mmfreelm_370M.onnx --saveEngine=mmfreelm_370M.engine --exportProfile=mmfreelm_370M.json --useSpinWait --separateProfileRun > mmfreelm_370M.log --verbose --profilingVerbosity=detailed --dumpProfile --fp16 --useCudaGraph
+$trtexec --onnx=mmfreelm_370M.onnx --int8 --saveEngine=mmfreelm_370M.engine > mmfreelm_370M.log --useCudaGraph
